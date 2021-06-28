7827e99769002cc3640550735160698498ae6464
fabirino <fabalexandre8@gmail.com>
2021-06-28 11:48:44 +0100

    le mas imprime duas vezes

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-#import config
 import socket
+from collections import namedtuple
+
+#import config
+
+Message = namedtuple(
+    'Message',
+    'prefix user channel irc_command irc_args text text_command text_args',
+
+)
 
 class Bot:
     def __init__(self):
         self.irc_server = 'irc.twitch.tv'
         self.irc_port = 6667
-        self.oauth_token = 'iq0aphsud3csuzq3ibryo6h4e9bwuj'
+        self.oauth_token = 'oauth:iq0aphsud3csuzq3ibryo6h4e9bwuj'
         self.username = 'yakaribot'
         self.channels = ['yakaribot']
 
@@ -14,7 +22,7 @@ class Bot:
 
     def send_command(self, command):
         if 'PASS' not in command:
-            print(f'<{command}') 
+            print(f'< {command}') 
         self.irc.send((command + '\r\n').encode())
 
     def connect(self):
@@ -27,16 +35,77 @@ class Bot:
             self.send_privmsg(channel, "Hey there!")
         self.loop_for_messages()
 
+
+    def get_user_from_prefix(self, prefix):
+        domain = prefix.split('!')[0]
+        if domain.endswith('.tmi.twitch.tv'):
+            return domain.replace('.tmi.twitch.tv', '')
+        if '.tmi.twitch.tv' not in domain:
+            return domain
+        return None
+   
+
+    def parse_mesage(self, received_msg):
+        parts = received_msg.split(' ')
+
+        prefix = None 
+        user = None 
+        channel = None 
+        text = None 
+        text_command = None 
+        text_args = None 
+        irc_command = None 
+        irc_args = None 
+
+        if parts[0].startswith(':'):
+            prefix = parts[0][1:]
+            user = self.get_user_from_prefix(prefix)
+            parts = parts[1:]
+
+        text_starts = next(
+            (idx for idx, part in enumerate(parts) if part.startswith(':')),
+            None
+        )
+        if text_starts is not None:
+            text_parts = parts[text_starts:]
+            text_parts[0] = text_parts[0][1:]
+            text = ' '.join(text_parts)
+            text_command = text_parts[0]
+            text_args = text_parts[1:]
+            parts = parts[:text_starts]
+
+        irc_command = parts[0]
+        irc_args = parts[1:]
+
+        hash_start = next(
+            (idx for idx, part in enumerate(irc_args) if part.startswith('#')),
+            None
+        )
+        if hash_start is not None:
+            channel = irc_args[hash_start][1:]
+
+        message = Message(
+            prefix=prefix,
+            user = user,
+            channel = channel,
+            text = text,
+            text_command = text_command,
+            text_args= text_args,
+            irc_command= irc_command,
+            irc_args= irc_args,
+        )
+        return message
+
     def handle_message(self, received_msg):
-        print(f'>{received_msg}')
+        message = self.parse_mesage(received_msg)
+        print(f'> {message}')
+
 
     def loop_for_messages(self):
         while True:
             received_msgs = self.irc.recv(2048).decode()
             for received_msg in received_msgs.split('\r\n'):
                 self.handle_message(received_msg)
-
-
 
 
 def main():
